6b7649fb7867a3e3d720522f6ccb947295d6f308
nanahocke <80675482+nanahocke@users.noreply.github.com>
2021-03-17 12:19:51 +0100

    Add files via upload

---
+++ xifs.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+
+def CRF(filename):
+    """loads netCDF file, calculates CRF on lat/lon grid, plots global mean in a time series"""
+    
+    ds=xr.open_dataset(filename)
+    
+    ###variables
+    ttrc=ds.ttrc
+    tsrc=ds.tsrc
+    ttr=ds.ttr
+    tsr=ds.tsr
+    
+    ###Calculation of CRF, is this correct?
+    CRF_LW=ttrc-ttr
+    CRF_SW=tsrc-tsr
+    CRF=CRF_SW+CRF_LW
+    CRF_lat_mean=CRF.mean('lat')
+    CRF_global_mean=CRF_lat_mean.mean('lon')/86400 #for unit in W/m²
+    
+    ###plotting
+    plt.figure(figsize=(15,5))
+    CRF_global_mean.plot()
+    plt.ylabel('CRF [$W/m^2$]')
+    plt.xlabel('Time')
+    plt.savefig(filename[:-3]+'.png')
