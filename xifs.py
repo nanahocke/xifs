f0b19772d6e83d935bc4568e4b5f89adcb6b6e08
nanahocke <80675482+nanahocke@users.noreply.github.com>
2021-03-22 16:17:46 +0100

    Create xifs.py

---
+++ xifs.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+import hvplot.xarray
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+import cartopy.crs as ccrs
+
+def CRF(filename):
+    """calculates CRF on lat/lon grid, plots global mean in a time series"""
+    
+    ds=xr.open_dataset(filename)
+    weights=np.cos(np.deg2rad(ds.lat))
+    
+    ###variables
+    ttrc=ds.ttrc
+    tsrc=ds.tsrc
+    ttr=ds.ttr
+    tsr=ds.tsr
+    
+    ###Calculation of CRF still wrong
+    CRF_LW=ttrc-ttr
+    CRF_SW=tsrc-tsr
+    CRF=CRF_SW+CRF_LW
+    CRF_weighted=CRF.weighted(weights)
+    CRF_global_mean=CRF_weighted.mean(('lat', 'lon'))/10800
+    
+    ###plotting
+    plt.figure(figsize=(15,5))
+    CRF_global_mean.plot()
+    plt.ylabel('CRF [$W/m^2$]')
+    plt.xlabel('Time')
+    plt.savefig(filename[:-3]+'_CRF.png')
+
+def output_variable(filename, var):
+    """input: netcdf data, variable; plots global mean in a time series"""
+    ds=xr.open_dataset(filename)
+    
+    variable=ds[var]
+    weights=np.cos(np.deg2rad(ds.lat))
+    var_weighted=variable.weighted(weights)
+    
+    var_global_mean=var_weighted.mean(('lat', 'lon'))
+    
+    ###plotting
+    plt.figure(figsize=(15,5))
+    var_global_mean.plot()
+    plt.ylabel(var+' ['+variable.attrs['units']+']')
+    plt.xlabel('Time')
+    plt.savefig(filename[:-3]+'_'+var+'.png')
+    
+def output_variable_seasonal_map(filename, var, seas):
+    """input: netcdf data, variable, season; plots seasonal average of variable as Mollweide projection"""
+    ds=xr.open_dataset(filename)
+    
+    month_length=ds[var].time_counter.dt.days_in_month
+    weights = month_length.groupby('time_counter.season') / month_length.groupby('time_counter.season').sum()
+    np.testing.assert_allclose(weights.groupby('time_counter.season').sum().values, np.ones(4))
+    ds_weighted = (ds[var] * weights).groupby('time_counter.season').sum(dim='time_counter')
+    
+    var_season=ds_weighted.sel(season=seas)
+    
+    ###plotting
+    plt.figure(figsize=(10,5))
+    p = var_season.plot(cmap='Spectral_r',
+        subplot_kws=dict(projection=ccrs.Mollweide(0), facecolor="gray"),
+        transform=ccrs.PlateCarree(),
+    )
+    plt.title(var+' ['+ds[var].attrs['units']+']\nseason='+seas)
+    p.axes.gridlines()
+    p.axes.coastlines()
+    plt.savefig(filename[:-3]+var+'_global.png')
